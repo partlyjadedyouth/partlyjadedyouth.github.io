1268322a746da272e4318dc9dcfccfe2f2fb06df
Joonyoung Park <mogun99@snu.ac.kr>
2023-01-11 14:45:58 +0900

    Modify: Blog title

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,7 +18,7 @@ function HomePage({ data }) {
 
   return (
     <Layout>
-      <Seo title="Home" />
+      <Seo title="partlyjadedyouth" />
       <Bio author={author} />
       <PostTabs
         posts={posts}
